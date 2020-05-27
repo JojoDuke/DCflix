eb4e69b300b4422e88263622174ff69d1e4b3e42
Duke Opoku-Amankwa <jojoamankwa@gmail.com>
2020-05-27 08:03:43 +0000

    Got Movies API working

--- DC_Project/ViewModels/MainWindowViewModel.cs
+++ DC_Project/ViewModels/MainWindowViewModel.cs
@@ -11,13 +11,30 @@ using System.Configuration;
 using System.Net.Http.Headers;
 using System.Windows.Input;
 using System.Windows;
+using System.Collections.ObjectModel;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.General;
 
 namespace DC_Project.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        #region Fields
+
         private Window mWindow;
+
+        #endregion
+
         #region Properties
+
+        public string MovieName { get; set; }
+
+        public string MovieDescription { get; set; }
+
+        public string MovieCast { get; set; }
+
+        public string MovieImage { get; set; }
 
         #endregion
 
@@ -52,7 +69,22 @@ namespace DC_Project.ViewModels
             MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
             MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
 
-            // On window being moved/dragged
+            //This is The Movie Database API Client
+            TMDbClient client = new TMDbClient("c807e25e9945dcb331636165896edb32");
+            Movie movie = client.GetMovieAsync("141052", MovieMethods.Credits | MovieMethods.Images).Result;
+
+            MovieName = movie.Title;
+            MovieDescription = movie.Overview;
+            foreach (ImageData image in movie.Images.Backdrops)
+            {
+                MovieImage = "https://image.tmdb.org/t/p/original"+image.FilePath;
+            }
+
+            foreach (Cast cast in movie.Credits.Cast)
+            {
+                MovieCast = cast.Name;
+            }
+
         }
         #endregion
     }
